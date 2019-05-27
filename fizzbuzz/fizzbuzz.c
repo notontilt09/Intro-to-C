8ce6577804fe0bc6085aa7fef973c2e1928ae793
Daniel Weinman <namniewd@gmail.com>
2019-05-27 17:41:43 -0400

    fizzbuzzing

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,27 @@
 */
 int fizzbuzz(int n)
 {
+    char fizzbuzz[10] = "Fizzbuzz";
+    char fizz[5] = "Fizz";
+    char buzz[5] = "Buzz";
+    int counter = 0;
 
+    for (int i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            printf("%s\n", fizzbuzz);
+        }
+        else if (i % 5 == 0) {
+            printf("%s\n", buzz);
+        }
+        else if (i % 3 == 0) {
+            printf("%s\n", fizz);
+        } 
+        else {
+            counter++;
+        }
+    }
+    printf("%d\n", counter);
+    return counter;
 }
 
 #ifndef TESTING
