096af6b68e9725c5cd835468764069393aae3eda
Daniel Weinman <namniewd@gmail.com>
2019-05-28 10:06:11 -0400

    queue almost working, queue.c running but tests not passing

--- queue/queue.c
+++ queue/queue.c
@@ -15,7 +15,12 @@ typedef struct Queue {
 */
 Queue *createQueue(unsigned capacity)
 {
+    Queue *q = malloc(sizeof(Queue));
+    q->length = 0;
+    q->capacity = capacity;
+    q->storage = (int *) malloc(q->capacity * sizeof(int));
 
+    return q;
 }
 
 /*
@@ -25,7 +30,18 @@ Queue *createQueue(unsigned capacity)
 */
 void enqueue(Queue *q, int item)
 {
-
+    // if there's still room at the end
+    if (q->capacity > q->length) {
+        // add item to end of storage
+        q->storage[q->length] = item;
+        // increase length of queue by 1
+        q->length++;
+    } else {
+        // reallocate memory to hold new item
+        resize_memory(q, q->capacity + sizeof(int));
+        q->storage[q->length] = item;
+        q->length++;
+    }
 }
 
 /*
@@ -33,8 +49,21 @@ void enqueue(Queue *q, int item)
     If the queue is empty, this function should return -1.
 */
 int dequeue(Queue *q)
-{
-
+{     
+    // if nothing in queue return -1
+    if (q->length == 0) {
+        return -1;
+    }
+    else {
+        // store the first value in storage
+        int pop = q->storage[0];
+        // decrement the length of the queue
+        q->length--;
+        // move the pointer to the next item in storage
+        q->storage++;
+        // return the old first value
+        return pop;
+    }
 }
 
 /*
@@ -43,7 +72,18 @@ int dequeue(Queue *q)
 */
 void destroyQueue(Queue *q)
 {
-
+    // move pointer to beginning of queue
+    q->storage = &q->storage[0];
+    // free storage at each index of queue
+    while(*q->storage) {
+        if (q->storage != NULL) {
+            free(q->storage);
+        }
+        q->storage++;
+    }
+    if (q != NULL) {
+        free(q);
+    }
 }
 
 
